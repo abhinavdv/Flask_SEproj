255ed772475f2c1edb54c683bc4f5ddd8e8bf8a6
abhinavdv <dvabhinav31@gmail.com>
2020-05-11 17:10:25 +0530

    Done with login, signup, initial pages

---
+++ Flask_SEproj/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager
+from flask_mail import Mail
+import os
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cc68a17d6d835225881c654a802e038b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_messageOcategory = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+
+from Flask_SEproj import routes
---
+++ run.py
@@ -0,0 +1,5 @@
+from Flask_SEproj import app
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
